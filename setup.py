9e49bb0cdfe9679c8e6495059fb32278c0a3c4c7
Nilavo Boral <96013573+NilavoBoral@users.noreply.github.com>
2025-09-22 14:34:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     description='An interactive application for testing MCP tools.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    url="https://github.com/NilavoBoral/mcp-judge",
     project_urls={
         "LinkedIn": "https://www.linkedin.com/in/nilavo-boral-123bb5228/"
     },
-    # url='https://github.com/yourusername/mcp-judge-app',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
